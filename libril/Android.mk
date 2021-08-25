LineageOS/android_hardware_ril

libril: Fix manual network selection with old modem
 * Old modem sends and applies mccmnc+network type, but new aosp telephony uses
   only digits.
 * Manually cut or append the "+" and strings after that to work around

Change-Id: I1015e6c0d6afdc0c03d70e9491d49331703eddcc

libril/Android.mk
@@ -41,6 +41,10 @@ ifneq ($(DISABLE_RILD_OEM_HOOK),)
     LOCAL_CFLAGS += -DOEM_HOOK_DISABLED
 endif
 
+ifneq ($(TARGET_USE_OLD_MNC_FORMAT),)
+    LOCAL_CFLAGS += -DOLD_MNC_FORMAT
+endif
+
 LOCAL_C_INCLUDES += external/nanopb-c
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../include

tiger-cave/hardware_ril

 * Old modem sends and applies mccmnc+network type, but new aosp
   telephony uses only digits.
 * Manually cut or append the "+" and strings after that to work around.

libril/Android.mk
@@ -38,6 +38,10 @@ ifneq ($(DISABLE_RILD_OEM_HOOK),)
     LOCAL_CFLAGS += -DOEM_HOOK_DISABLED
 endif
 
+ifneq ($(TARGET_USES_OLD_MNC_FORMAT),)
+    LOCAL_CFLAGS += -DOLD_MNC_FORMAT
+endif
+
 LOCAL_C_INCLUDES += external/nanopb-c
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../include